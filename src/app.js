587b268367e016503f99aaac31a6ba1a2fb737f3
camilo barrera <camilobarreraj@gmail.com>
2025-04-08 18:21:42 -0500

    Enrutador y controladores

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import clientesRoutes from "./routes/clientes.routes.js";
 
 const app = express();
 
 app.set("port", 5000)
+
+app.use("/api/clientes", clientesRoutes);
 
 export default app;
---
+++ src/controllers/clientes.controllers.js
@@ -0,0 +1,7 @@
+const getClientes = (req, res) => {
+    res.json({"Clientes": "marisol"})
+}
+
+export const methodHTTP = {
+    getClientes
+}
