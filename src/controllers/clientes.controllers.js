eff3a59dd8c1ec36693933f11537f78fc9eee63b
camilo barrera <camilobarreraj@gmail.com>
2025-04-08 18:49:28 -0500

    Connecting DB - HTTPGET

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,5 +1,14 @@
-const getClientes = (req, res) => {
-    res.json({"Clientes": "marisol"})
+import getConnection from "./../db/database.js";
+
+const getClientes = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id_cliente, nombre, telefono, correo, direccion FROM clientes")
+        res.json(result)  
+    } catch (error) {
+        console.error("ERROR 500");
+    }
+    
 }
 
 export const methodHTTP = {
